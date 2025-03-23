973cb09c3e278dbe88dee0903dd3c5a7256bdbf1
Enmanuel Urbina <enmanuelu100@gmail.com>
2025-03-23 00:46:59 -0600

    uso de estados en react

--- 01-hola-mundo/src/App.jsx
+++ 01-hola-mundo/src/App.jsx
@@ -5,17 +5,14 @@ export function App () {
     return(
         <section className='App'>
             <TwitterFollowCard 
-                isFollowing
                 userName="midudev" 
                 name="jose Enmanuel Urbina" 
             />
             <TwitterFollowCard 
-                isFollowing={false} 
                 userName="vattistue" 
                 name = "que pas chaval" 
             />
             <TwitterFollowCard 
-                isFollowing 
                 userName="pheralb" 
                 name="Jeff Bar" 
             />
--- 01-hola-mundo/src/TwitterFollowCard.jsx
+++ 01-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,21 +1,27 @@
-export function TwitterFollowCard ({userName, name, isFollowing}) {
-    const imageSrc = `https://unavatar.io/${userName}` 
+import { useState } from 'react'
+export function TwitterFollowCard ({children,userName}) { 
+
+    const [isFollowing, setIsFollowing] = useState(false)
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const handleFollowClick = () => { setIsFollowing(!isFollowing) }
+
+    const buttonClassName = isFollowing ? 'tw-followCard-button isFollowing' : 'tw-followCard-button'
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img 
                 className='tw-followCard-avatar'
-                src={imageSrc} 
+                src={`https://unavatar.io/${userName}`} 
                 alt="img de midudev"/>
             <div className='tw-followCard-info'>
-                <strong> {name} </strong>
+                <strong> {children} </strong>
                 <span className='tw-followCard-infoUserName'>{userName} </span>
             </div>
             </header>
 
             <aside>
-                <button className='tw-followCard-button'>
-                    seguir    
+                <button className={buttonClassName} onClick= {handleFollowClick}>
+                    {text}    
                 </button>
             </aside>
         </article>
